b87ccd944013b7a9ed5b459645d8b1ff10dea65f
fraserxu <xvfeng123@gmail.com>
2014-09-11 11:15:35 +0800

    clean up map.js

--- src/js/map/map.js
+++ src/js/map/map.js
@@ -18,106 +18,131 @@ module.exports = React.createClass({
     return {
       csv_data: {},
       geo_data: [],
-      map: {}
+      map: {},
+      indicator: 'Indicator'
     };
   },
   componentWillMount: function() {
     var self = this;
-
-    self.setState({indicator: 'Indicator'})
     xhr({ responseType: 'arraybuffer', url: csv}, csv_response)
     xhr({ responseType: 'json', url: geo}, geo_response)
 
+    /**
+     * save geo response
+     * @param  {Object} err  err Object
+     * @param  {Object} resp response
+     * @param  {Object} data data
+     */
     function geo_response(err, resp, data) {
       self.setState({geo_data: data.features})
     }
 
+    /**
+     * save csv response
+     * @param  {Object} err  err
+     * @param  {Object} resp response
+     * @param  {Object} data data
+     */
     function csv_response(err, resp, data) {
       if (err) throw err
       var buff = new Buffer(new Uint8Array(data))
       var parser = bcsv({json: true})
-      parser.pipe(concat(render))
+      parser.pipe(concat(_render))
       parser.write(buff)
       parser.end()
     }
 
-    function render(rows) {
+    function _render(rows) {
       var dataByCountry = {};
-      for (var row_i in rows){
-          var countryName = rows[row_i]['Country'];
-          dataByCountry[countryName] = rows[row_i];
-          for (var indicatorName in Object.keys(dataByCountry[countryName])){
-              if (indicatorName.toLowerCase() != 'country'){
-                  dataByCountry[countryName][indicatorName] = Number(dataByCountry[countryName][indicatorName]);
-              }
+      rows.forEach(function(row) {
+        var countryName = row['Country']
+        dataByCountry[countryName] = row
+        Object.keys(dataByCountry[countryName]).forEach(function(indicatorName) {
+          if (indicatorName.toLowerCase() != 'country'){
+            dataByCountry[countryName][indicatorName] = Number(dataByCountry[countryName][indicatorName]);
           }
-      }
+        })
+      })
       self.setState({csv_data: dataByCountry});
     }
   },
+
+  /**
+   * load map box instance
+   * and mount to state
+   */
   componentDidMount: function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiZnJhc2VyeHUiLCJhIjoiZ2toZEJhayJ9.im7zAkjGosi1fFKB3PYD2Q'
     var map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([42, 9.56], 4)
     this.setState({map: map})
   },
   componentWillUpdate: function(nextProps, nextState) {
-    var getColor = function(value){
-        if (!value) return 'rgba(0,0,0,.05)';
-        if (value >= nextState.ranges.ranges[5]) return '#5B79AD';
-        if (value >= nextState.ranges.ranges[4]) return '#6884B3';
-        if (value >= nextState.ranges.ranges[3]) return '#6B87B4';
-        if (value >= nextState.ranges.ranges[2]) return '#819BC3';
-        if (value >= nextState.ranges.ranges[1]) return '#94AACB';
-        if (value >= nextState.ranges.ranges[0]) return '#A7BAD2';
+
+    /**
+     * get color from range
+     * @param  {Number} value color value
+     * @return {String}       color code
+     */
+    function getColor(value){
+      if (!value) return 'rgba(0,0,0,.0)';
+      if (value >= nextState.ranges.ranges[5]) return '#5B79AD';
+      if (value >= nextState.ranges.ranges[4]) return '#6884B3';
+      if (value >= nextState.ranges.ranges[3]) return '#6B87B4';
+      if (value >= nextState.ranges.ranges[2]) return '#819BC3';
+      if (value >= nextState.ranges.ranges[1]) return '#94AACB';
+      if (value >= nextState.ranges.ranges[0]) return '#A7BAD2';
     }
 
-    var getStyle = function(feature){
-        var valuez;
-        var countryName = feature.properties.name;
-        // console.log('feature', feature, nextState.feature);
-        if (nextState.csv_data[countryName] && nextState.csv_data[countryName][nextState.indicator]){
-            console.log('set', nextState.csv_data[countryName][nextState.indicator])
-            valuez = nextState.csv_data[countryName][nextState.indicator];
-            console.log('set', valuez, nextState.csv_data[countryName][nextState.indicator])
-        }
-        var col = getColor(valuez);
-        console.log('color is', col, valuez);
-        return {
-            weight: 1.2,
-            opacity: 1,
-            fillOpacity: 1,
-            fillColor: col
-        }
+    /**
+     * get style from feature
+     * @param  {Object} feature object
+     * @return {Object} style object
+     */
+    function getStyle(feature){
+      var valuez;
+      var countryName = feature.properties.name;
+      if (nextState.csv_data[countryName] && nextState.csv_data[countryName][nextState.indicator]){
+          valuez = nextState.csv_data[countryName][nextState.indicator];
+      }
+      var col = getColor(valuez);
+      return {
+          weight: 1.2,
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: col
+      }
     };
 
-    var onEachFeature = function(feature){
+    function onEachFeature(feature){
       //console.log(feature.properties.name);
     };
-    
-    var getRanges = function(data, indicator){
-        debugger;
-        var values = [];
-        for (var i in data){
-            console.log(data[i])
-            if (data[i][indicator]){
-                values.push(data[i][indicator]);
-            }
-        }
 
-        var max = Math.max.apply(Math, values)
-        var min = Math.min.apply(Math, values)
+    /**
+     * get ranges from data and indicator
+     * @param  {Object} data      data
+     * @param  {String} indicator string
+     * @return {Object}           ranges object
+     */
+    function getRanges(data, indicator){
+      var values = Object.keys(data).map(function(row) {
+        if (data[row][indicator]) return data[row][indicator]
+      })
 
-        var rangePoints = [];
-        var step = (max - min)/colorCount;
-        for (var i = 0; i < colorCount; i++){
-          rangePoints.push(min + i*step);
-        }
+      var max = Math.max.apply(Math, values)
+      var min = Math.min.apply(Math, values)
 
-        return {min: min, max: max, ranges: rangePoints};
+      var rangePoints = [];
+      var step = (max - min)/colorCount;
+      for (var i = 0; i < colorCount; i++){
+        rangePoints.push(min + i*step);
+      }
+
+      return {min: min, max: max, ranges: rangePoints};
     }
 
-    console.log('nextState', nextState)
-    
+    /**
+     * Make sure we have the data to add to the map
+     */
     if (Object.keys(nextState.csv_data).length >0 && nextState.geo_data.length >0){
       var ranges = getRanges(nextState.csv_data, nextState.indicator);
       nextState.ranges = ranges;
@@ -126,7 +151,6 @@ module.exports = React.createClass({
         style: getStyle,
         onEachFeature: onEachFeature
       }).addTo(nextState.map);
-      
     }
   },
   render: function() {
