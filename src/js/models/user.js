d9f49d15afeafd8aed09abbf8b234857bbc4a1b0
fraserxu <xvfeng123@gmail.com>
2014-09-14 12:13:04 +0800

    use 2 space instead of 2 Mohhahahaahh

--- src/js/models/user.js
+++ src/js/models/user.js
@@ -1,52 +1,46 @@
 var Promise = require('es6-promise').Promise,
-    xhr = require('xhr');
+  xhr = require('xhr');
 
 function User() {}
 
 User.prototype.get = function() {
-    this.token(window.localStorage.getItem('token'))
-    var self = this;
-    return new Promise(function(resolve, reject) {
-        if (!self._token)
-            return reject(new Error('401'));
-        xhr({
-                uri: 'http://csviz.dev.wiredcraft.com/user',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + self._token
-                }
-            },
-            function(err, resp, body) {
-                if(err)
-                    return reject(err);
-                if (resp.statusCode === 200) {
-                    self.attrs = body
-                }
-                resolve(resp);
-            });
+  this.token(window.localStorage.getItem('token'))
+  var self = this;
+  return new Promise(function(resolve, reject) {
+    if (!self._token)
+      return reject(new Error('401'));
+    xhr({
+      uri: 'http://csviz.dev.wiredcraft.com/user',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + self._token
+      }
+    },
+    function(err, resp, body) {
+      if(err) return reject(err);
+      if (resp.statusCode === 200) self.attrs = body
+      resolve(resp);
     });
+  });
 }
 
 User.prototype.token = function(token) {
-    if (!token)
-        return this._token;
-    this._token = token;
-    return this;
+  if (!token) return this._token;
+  this._token = token;
+  return this;
 }
 
 User.prototype.save = function () {
-    if(this._token)
-        window.localStorage.setItem('token', this._token);
-    return this;
+  if(this._token) window.localStorage.setItem('token', this._token);
+  return this;
 }
 
 User.prototype.clear = function() {
-    this._token = null;
-    this.attrs = null
-    window.localStorage.clear();
-    return this;
+  this._token = null;
+  this.attrs = null
+  window.localStorage.clear();
+  return this;
 }
-
 
 module.exports = exports._user || (exports._user = new User())
 
