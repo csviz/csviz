dbb46598749e4489c7be55e0e4eb203b51c598d8
fraserxu <xvfeng123@gmail.com>
2014-09-15 12:53:08 +0800

    add static files

---
+++ src/js/statics/notfound.js
@@ -0,0 +1,13 @@
+/**
+ * @jsx React.DOM
+ */
+
+var notFound = React.createClass({
+  render: function() {
+    return (
+      <div>Panda not found!</div>
+    );
+  }
+})
+
+module.exports = notFound
