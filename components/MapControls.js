1fd5882f6c4f73b74b595cf4d7b3057ab6207917
fraserxu <xvfeng123@gmail.com>
2014-11-05 14:57:14 +0800

    done with average component #13

--- components/MapControls.js
+++ components/MapControls.js
@@ -7,6 +7,7 @@ var IndicatorSelector = require('./IndicatorSelector')
 var SocialPanel = require('./SocialPanel')
 var Graph = require('./Graph')
 var Timeline = require('./Timeline')
+var Average = require('./Average')
 var GLOBALStore = require('../stores/GLOBALStore')
 
 var MapControls = React.createClass({
@@ -19,11 +20,19 @@ var MapControls = React.createClass({
     return (
       <div className='sidebar'>
         <ControlHeader configs={this.props.configs} />
+
         <IndicatorSelector configs={this.props.configs} />
+
         {  selected_indicator === 'gdp' ?
           <Timeline /> : null
         }
+
         <SocialPanel />
+
+        { selected_indicator === 'poverty' ?
+          <Average /> : null
+        }
+
         <Graph configs={this.props.configs} />
       </div>
     )
