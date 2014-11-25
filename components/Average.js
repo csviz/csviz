d2e883453970c784932e9d727f6ff49279af5f87
fraserxu <xvfeng123@gmail.com>
2014-11-25 13:23:29 +0800

    add sparkline component

--- components/Average.js
+++ components/Average.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var React = require('react')
-var Chartist = require('react-chartist')
+var Sparkline = require('react-sparkline')
 var GLOBALStore = require('../stores/GLOBALStore')
 var _ = require('lodash')
 var numeral = require('numeral')
@@ -21,6 +21,7 @@ var Average = React.createClass({
     var Chart = null
     var values = []
     var countryList = null
+    var sl = null
 
     if (!_.isEmpty(selected_indicator) && !_.isEmpty(globals)) {
       if (!_.isEmpty(configs) && configs.indicators[selected_indicator].years) {
@@ -30,10 +31,15 @@ var Average = React.createClass({
 
         countryList = Object.keys(globals.data.locations).map(function(countryName, key) {
           var formattedValue = numeral(globals.data.locations[countryName][selected_indicator].years[selected_year]).format('0,0')
+          var countryData = _.map(globals.data.locations[countryName][selected_indicator].years, function(value) {
+            return value
+          })
+          var countryChart = <Sparkline data={countryData} />
 
           return (
             <li key={key} className='countryItem'>
               <span className='label'>{globals.meta.locations[countryName].label}</span>
+              <span className='chart'>{countryChart}</span>
               <span className='value'>{formattedValue}</span>
             </li>
           )
@@ -44,22 +50,7 @@ var Average = React.createClass({
           return (value.toFixed(2))/1000
         })
 
-        var barChartData = {
-          labels: globals.meta.indicators[selected_indicator].years,
-          series: [dataSeries]
-        }
-        var barChartOptions = {
-          showArea: true,
-          low: (globals.meta.indicators[selected_indicator].avg.years[selected_year].toFixed(2))/1000,
-          axisY: {
-            labelInterpolationFnc(value) {
-              return numeral(value).format('0 a')
-            }
-          }
-        }
-
-
-        Chart = <Chartist type={'Line'} data={barChartData} options={barChartOptions} />
+        Chart = <Sparkline data={dataSeries} />
       } else {
         values = _.map(globals.data.locations, selected_indicator)
         average = globals.meta.indicators[selected_indicator].avg.toFixed(2)
@@ -70,9 +61,12 @@ var Average = React.createClass({
     return (
       <div className='card'>
         <div className='average-box'>
-          <h5>AVERAGE: {average}</h5>
-          <div className='chart'>
-            {Chart}
+          <div className='average-box-header'>
+            <span className='label'>AVERAGE</span>
+            <div className='chart'>
+              {Chart}
+            </div>
+            <span className='value'>{average}</span>
           </div>
           <div className='average-chart'>
             <ul className='country-list'>
