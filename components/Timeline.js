de8c4cca8f644b04ea7b12183aee16438412458a
fraserxu <xvfeng123@gmail.com>
2014-12-02 18:27:20 +0800

    add play buttonto timeline

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var React = require('react')
+var _ = require('lodash')
+
 var MapActionCreators = require('../actions/MapActionCreators')
 var Store = require('../stores/Store')
 var createStoreMixin = require('../mixins/createStoreMixin')
@@ -10,6 +12,13 @@ var Timeline = React.createClass({
   displayName: 'Timeline',
 
   mixins: [createStoreMixin(Store)],
+
+  getInitialState() {
+    return {
+      isTimelinePlaying: false,
+      playLoop: null
+    }
+  },
 
   getStateFromStores() {
     var selected_indicator = Store.getSelectedIndicator()
@@ -28,6 +37,10 @@ var Timeline = React.createClass({
     this.setState(this.getStateFromStores())
   },
 
+  componentWillUnmount() {
+    if (this.state.playLoop) window.clearInterval(this.state.playLoop)
+  },
+
   handleStoreChange() {
     this.setState(this.getStateFromStores())
   },
@@ -37,8 +50,31 @@ var Timeline = React.createClass({
     MapActionCreators.changeSelectedYear(selected_year)
   },
 
+  playTimeline() {
+    var selected_indicator = Store.getSelectedIndicator()
+    this.setState({ isTimelinePlaying: !this.state.isTimelinePlaying })
+
+    if (this.state.isTimelinePlaying) {
+      if (this.state.playLoop) window.clearInterval(this.state.playLoop)
+    } else if (!this.state.isTimelinePlaying) {
+      var playLoop = window.setInterval(function() {
+        var current_year = parseInt(Store.getSelectedYear())
+        if (current_year >= parseInt(_.last(this.props.data.global.meta.indicators[selected_indicator].years))) {
+          window.clearInterval(playLoop)
+          this.setState({ isTimelinePlaying: false })
+        } else {
+          var next_year = current_year + 1
+          MapActionCreators.changeSelectedYear(next_year)
+        }
+      }.bind(this), 1000)
+
+      this.setState({ playLoop: playLoop })
+    }
+
+  },
+
   render() {
-    var timeline = null
+    var timeline, playButton
     var selected_indicator = Store.getSelectedIndicator()
     var selected_year = Store.getSelectedYear()
 
@@ -46,12 +82,22 @@ var Timeline = React.createClass({
       timeline = this.props.data.global.meta.indicators[selected_indicator].years.map(function(year) {
         return <li key={year} value={year} className={ (year == selected_year) ? 'active' : null } onClick={this.handleYearClick}>{year}</li>
       }, this)
+
+      playButton = (
+        <li className='play-button' onClick={this.playTimeline}>
+          { this.state.isTimelinePlaying ?
+            'Stop' :
+            'Play'
+          }
+        </li>
+      )
     }
 
     return (
       <div className='timeline-box'>
         <ul className='timeline'>
-          { timeline }
+          {playButton}
+          {timeline}
         </ul>
       </div>
     )
