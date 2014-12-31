2bdbc2e8516c39ef153bbe8c0171c65d9ffb6b5f
fraserxu <xvfeng123@gmail.com>
2014-12-31 12:42:32 +0800

    update queryMixin

--- mixins/queryMixin.js
+++ mixins/queryMixin.js
@@ -1,8 +1,11 @@
 'use strict'
+
 var objectAssign = require('object-assign')
 
 var updateQueryMixin = {
   updateQuery(data) {
+    if(typeof data != 'object') throw new Error('query should be an object')
+
     var queries = this.getQuery()
     var _queries = objectAssign(queries, data)
     this.replaceWith('app', {}, _queries)
