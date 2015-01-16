674206b2c169ddf417fe1695ddb00a3f55797cc6
fraserxu <xvfeng123@gmail.com>
2015-01-16 12:10:12 +0800

    make sure bartchart is using int/float instead of string

--- components/BarchartEnvelope.js
+++ components/BarchartEnvelope.js
@@ -9,24 +9,15 @@ var BarchartEnvelope = React.createClass({
       width: 700,
       height: 200,
       strokeColor: 'black',
-      strokeWidth: '0.5px',
-      data: [15, 12, 25, 8, 20]
+      strokeWidth: '0.5px'
     }
   },
 
-  componentDidMount: function() {
-    this.renderBarchart()
+  componentWillReceiveProps(nextProps) {
+    this.renderBarchart(nextProps)
   },
 
-  componentDidUpdate: function() {
-    this.renderBarchart()
-  },
-
-  render: function() {
-    return React.createElement('div', null)
-  },
-
-  renderBarchart: function() {
+  renderBarchart: function(nextProps) {
     var el = this.getDOMNode()
     while (el.firstChild) {
       el.removeChild(el.firstChild)
@@ -34,12 +25,10 @@ var BarchartEnvelope = React.createClass({
 
     var width = this.props.width
     var height = this.props.height
-    var data = this.props.data.slice()
-    var onBarchartClick = this.props.onBarchartClick || (function() {})
-    var selectedIndex = this.props.selectedIndex
-    var hoverEffect = this.props.hoverEffect || false
-    var tooltip = this.props.tooltip || false
-    if (data.length === 0) return
+    var data = nextProps.data.map(function(value) {
+      return parseFloat(value)
+    })
+    if (!data) return
 
     var xScale = d3.scale.linear()
       .domain([0, data.length])
@@ -60,64 +49,29 @@ var BarchartEnvelope = React.createClass({
       .append('g')
 
     svg.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', function(d, i) {
-          return xScale(i)
-        })
-        .attr('y', function(d) {
-          return height - barchartScale(d)
-        })
-        // the width of the rect is actually calculated from the muted array
-        .attr('width', function() {
-          return width / data.length - 5
-        })
-        .attr('height', function(d) {
-          return barchartScale(d)
-        })
-        .attr('fill', function(d, i) {
-          // if (selectedIndex === i) {
-            return '#ccc'
-          // } else {
-            // return 'rgba(0, 0, 0, 0)'
-          // }
-        })
-        .style('cursor', 'pointer')
-        .on('mouseover', function(d) {
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', function(d, i) {
+        return xScale(i)
+      })
+      .attr('y', function(d) {
+        return height - barchartScale(d)
+      })
+      .attr('width', function() {
+        return width / data.length - 5
+      })
+      .attr('height', function(d) {
+        return barchartScale(d)
+      })
+      .attr('fill', function(d, i) {
+        return '#ccc'
+      })
+      .style('cursor', 'pointer')
+  },
 
-          if (tooltip) {
-            var xPosition = parseFloat(d3.select(this).attr('x')) + width / data.length / 2
-            var yPosition = parseFloat(d3.select(this).attr('y')) - 4
-            svg.append('text')
-              .attr('id', 'tooltip')
-              .attr('x', xPosition)
-              .attr('y', yPosition)
-              .attr('text-anchor', 'middle')
-              .attr('font-family', 'sans-serif')
-              .attr('font-size', '10px')
-              .attr('font-weight', 'bold')
-              .style('pointer-events', 'none')
-              .attr('fill', '#000')
-              .text(d)
-          }
-
-        })
-        .on('mouseout', function() {
-
-          if (tooltip) {
-            d3.select('#tooltip').remove()
-          }
-        })
-        .on('click', function(d, i) {
-          var g = d3.select(this.parentElement).selectAll('rect')
-            .attr('fill', 'rgba(0, 0, 0, 0)')
-
-          d3.select(this)
-            .attr('fill', 'orange')
-
-          onBarchartClick(d, i)
-        })
+  render: function() {
+    return React.createElement('div', null)
   }
 })
 
