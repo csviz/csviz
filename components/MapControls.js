154927096d909a7c62d2f47f8baba2d8b5994ece
fraserxu <xvfeng123@gmail.com>
2015-01-26 12:00:56 +0800

    add a sidebar toggle

--- components/MapControls.js
+++ components/MapControls.js
@@ -11,14 +11,50 @@ var MapControls = React.createClass({
 
   displayName: 'MapControls',
 
+  hasClass(el, className) {
+    if (el.classList) {
+      return el.classList.contains(className)
+    } else {
+      return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className)
+    }
+  },
+
+  addClass(el, className) {
+    if (el.classList) {
+      el.classList.add(className)
+    } else {
+      el.className += ' ' + className
+    }
+  },
+
+  removeClass(el, className) {
+    if (el.classList) {
+      el.classList.remove(className)
+    } else {
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
+    }
+  },
+
+  toggleSidebar() {
+    var body = document.querySelector('body')
+    if(this.hasClass(body, 'isSidebarOpen')) {
+      this.removeClass(body, 'isSidebarOpen')
+    } else {
+      this.addClass(body, 'isSidebarOpen')
+    }
+  },
+
   render() {
     return (
       <aside id='sidebar'>
-      <div className='sidebar-panel'>
-        <ControlHeader data={this.props.data} />
-        <IndicatorSelector data={this.props.data} />
-        <SocialPanel data={this.props.data} />
-        <Average data={this.props.data} />
+        <div className='sidebar-panel'>
+          <div className='sidebar-toggle'>
+            <button name='button' onClick={this.toggleSidebar}>Toggle Sidebar</button>
+          </div>
+          <ControlHeader data={this.props.data} />
+          <IndicatorSelector data={this.props.data} />
+          <SocialPanel data={this.props.data} />
+          <Average data={this.props.data} />
         </div>
       </aside>
     )
